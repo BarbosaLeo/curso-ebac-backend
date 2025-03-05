11558959479efcf3c138263a7f747266c25a565c
Leonardo Barbosa <leonardo8544@gmail.com>
2025-03-05 11:07:39 -0300

    update-main

---
+++ exercicio-generics/src/main/java/com/barbosaleo/generics/Main.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.barbosaleo.generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author leona
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+        Veiculos<Veiculo> veiculoList = new Veiculos<>();
+        veiculoList.addVeiculo(new Honda(1));
+        veiculoList.addVeiculo(new Honda(3));
+        veiculoList.addVeiculo(new Chevrolet(45));
+        
+        System.out.println(veiculoList.getMarcaVeiculo(1));
+        System.out.println(veiculoList.getMarcaVeiculo(45));
+        
+    }
+}
